bc113524531bd90fb17215f250f974074c22772d
Mansoor Barri <mansoor.eb.ak@gmail.com>
2024-04-16 17:35:11 +0100

    update: accessibility settings; add normal font size && cookies

--- static/accessibility.js
+++ static/accessibility.js
@@ -1,7 +1,53 @@
-function increaseFontSize() {
-    document.body.style.fontSize = "larger";
+
+// Function to set a cookie
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
 
+// Function to get a cookie value by name
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1, cookie.length);
+        }
+        if (cookie.indexOf(nameEQ) === 0) {
+            return cookie.substring(nameEQ.length, cookie.length);
+        }
+    }
+    return null;
+}
+
+// Function to increase font size
+function increaseFontSize() {
+    document.body.style.fontSize = "larger";
+    setCookie("fontSize", "larger", 365); // Set cookie for 1 year
+}
+
+// Function to decrease font size
 function decreaseFontSize() {
     document.body.style.fontSize = "smaller";
+    setCookie("fontSize", "smaller", 365); // Set cookie for 1 year
 }
+
+// Function to reset font size to default
+function resetFontSize() {
+    document.body.style.fontSize = ""; // Reset to default
+    setCookie("fontSize", "", -1); // Expire the cookie to remove it
+}
+
+// When the page loads, check if there's a font size cookie and apply it
+window.onload = function () {
+    var fontSize = getCookie("fontSize");
+    if (fontSize) {
+        document.body.style.fontSize = fontSize;
+    }
+};
